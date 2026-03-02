7fdb8482b145fb259fe5c1dfce5f7cba7f618f38
LeahBasson <leahbasson54@gmail.com>
2026-03-02 21:16:21 +0200

    :tada:

--- Prac4/src/za/ac/cput/Prac4.java
+++ Prac4/src/za/ac/cput/Prac4.java
@@ -1,10 +1,13 @@
 package za.ac.cput;
 
 import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
 import javax.swing.*;
 
 public class Prac4
-        extends JFrame {
+        extends JFrame
+        implements ActionListener {
 
     // have to get this part
     // labels
@@ -13,6 +16,8 @@ public class Prac4
     private JLabel mobileLbl;
     private JLabel studLbl;
     private JLabel addressLbl;
+    private JLabel checkBoxLbl;
+    private JLabel buttonsLbl;
 
     // textfields
     private JTextField nameTxt;
@@ -32,69 +37,139 @@ public class Prac4
 
     //checkbox
     private JCheckBox messageBox;
-    
-    // panels
     private JPanel panel1;
     private JPanel panel2;
     private JPanel panel3;
     private JPanel panel4;
 
+    // buttons
+    private JButton submitBtn;
+    private JButton resetBtn;
+
     // constructor
     public Prac4() {
-        //call parent class constructor
-        // set frame title
+        //**call parent class constructor
         super("Student Registration");
 
-        // layout of the frame: rows, cols
-        setLayout(new GridLayout(2, 1));
-
+        /**
+         * layout of the frame
+         */
+        setLayout(new GridLayout(1, 2));
         // have to get this part
         // a box within the JFrame
         panel1 = new JPanel();
         panel2 = new JPanel();
         panel3 = new JPanel();
         panel4 = new JPanel();
-        panel1.setLayout(new FlowLayout());
-        panel2.setLayout(new GridLayout(1, 2));
-        panel3.setLayout(new FlowLayout(FlowLayout.CENTER, 2, 2));
 
+        // rows, cols
+        panel1.setLayout(new GridLayout(6, 2));
+        panel2.setLayout(new FlowLayout());
+        panel3.setLayout(new FlowLayout());
+        panel4.setLayout(new FlowLayout());
+        
+        // labels     
+        nameLbl = new JLabel("Name");
         headingLbl = new JLabel("Student Registration");
+        mobileLbl = new JLabel("Mobile");
+        studLbl = new JLabel("Student");
+        addressLbl = new JLabel("Address");
+        checkBoxLbl = new JLabel(" ");
+        buttonsLbl = new JLabel("");
+
+        // textfields
+        nameTxt = new JTextField(10);
+        mobileTxt = new JTextField(10);
+
+        // radio buttons
+        underGrdRadio = new JRadioButton("undergraduate");
+        postGrdRadio = new JRadioButton("postgraduate");
+        group = new ButtonGroup();
+
+        // text area
+        addressTxt = new JTextArea(10, 40);
+        messageTxt = new JTextArea(10, 40);
+
+        // checkbox
+        messageBox = new JCheckBox("Accept Terms and Conditions.");
+
+        // buttons
+        submitBtn = new JButton("Submit");
+        resetBtn = new JButton("Reset");
+
+        // have to get this part
+        //add(headingLbl);
+        panel1.add(nameLbl);
+        panel1.add(nameTxt);
+        panel1.add(mobileLbl);
+        panel1.add(mobileTxt);
+
+        panel1.add(studLbl);
+        // allows you to only select 1 at a time.
+        group.add(underGrdRadio);
+        group.add(postGrdRadio);
+        
+        panel3.add(underGrdRadio);
+        panel3.add(postGrdRadio);
+        panel1.add(panel3);
+        
+
+        panel1.add(addressLbl);
+        panel1.add(addressTxt);
+        panel1.add(checkBoxLbl);
+        panel1.add(messageBox);
+       
+        panel1.add(buttonsLbl);
+        panel4.add(submitBtn);
+        panel4.add(resetBtn);
+        panel1.add(panel4);
+        
+        add(panel1);
+
+        submitBtn.addActionListener(this);
+        resetBtn.addActionListener(this);
+
+        panel2.add(messageTxt);
+        add(panel2);
+
         // style heading label
         headingLbl.setFont(new Font("Arial", Font.PLAIN, 24));
-        
-        nameTxt = new JTextField(15);
-        nameLbl = new JLabel("Name"); 
+        messageTxt.setEnabled(false);
 
-        // adding content
-        //add(panel1);
-        add(headingLbl);
-        add(panel2);
-        panel2.add(panel3);
-        panel2.add(panel4);
-        
-        panel3.add(nameLbl);
-        panel3.add(nameTxt);
-
-        panel1.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        panel2.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        panel3.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        panel4.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-
-//        nameTxt = new JTextField(15);
-//        nameLbl = new JLabel("Name");
-//
-//        mobileLbl = new JLabel("Mobile");
-//        mobileTxt = new JTextField(15);
-//        add(mobileLbl);
-//        add(mobileTxt);
-//        
-//        messageTxt = new JTextArea(38, 24);
-//        
-//        studLbl = new JLabel("Student");
-        // have to get this part
-//        add(panel1);
-//        add(panel2);
-//        add(messageTxt);
     }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // Display all the user-entered values into the textarea
+        if (e.getSource() == resetBtn) {
+            nameTxt.setText("");
+            mobileTxt.setText("");
+            // FIX: apply the functionality to the button group.
+            group.clearSelection();
+
+            addressTxt.setText("");
+            messageBox.setSelected(false);
+            messageTxt.setText("");
+
+        }
+
+        if (e.getSource() == submitBtn) {
+
+            if (underGrdRadio.isSelected()) {
+                messageTxt.setText(
+                        nameTxt.getText() + "\n" + mobileTxt.
+                        getText() + "\n" + underGrdRadio.getText() + "\n" + addressTxt.
+                        getText());
+            }
+
+            if (postGrdRadio.isSelected()) {
+                messageTxt.setText(
+                        nameTxt.getText() + "\n" + mobileTxt.
+                        getText() + "\n" + postGrdRadio.getText() + "\n" + addressTxt.
+                        getText());
+            }
+
+        }
+
+    }
 }// end of class
